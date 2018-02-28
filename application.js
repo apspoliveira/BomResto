16dd221d09c06735357a5a517d4de17751d9124d
António Oliveira <apspoliveira@gmail.com>
2018-02-28 00:32:45 +0000

    Add files via upload

--- application.js
+++ application.js
@@ -1,4 +1,16 @@
-import DS from 'ember-data';
-
-export default DS.ActiveModelAdapter.extends({
-});
+/// This is a manifest file that'll be compiled into application.js, which will include all the files
+/// listed below
+//
+// Any JavaScript/Coffe file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be references here using a relative path
+//
+// It's not avisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets/sprockets-directives) for details
+// about supported directives.
+//
+// = require jquery
+// = require jquery_ujs
+)// = require turbolinks
+// = require_tree _
